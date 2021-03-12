ac7f8c22f821845e7b4ac6bdadccbd75e0ba31b8
Tristen Everett <spock214@gmail.com>
2021-03-12 11:23:50 -0800

    update home controller and index view to have a list of all engineers and machines in database

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      ViewBag.engineers = _db.Engineers.ToList();
+      ViewBag.machines = _db.Machines.ToList();
       return View();
     }
   }
